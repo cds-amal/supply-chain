b8633a3b2441319268c7057cd6c5b52014fa5456
Amal Sudama <cds.sudama@gmail.com>
2018-07-01 06:55:51 -0400

    first attempt

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -1,13 +1,75 @@
-pragma solidity ^0.4.13;
+pragma solidity ^0.4.21;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
 
+
 contract TestSupplyChain {
+    // uint public initialBalance = 100;
+    uint public initialBalance;
+    SupplyChain public chain;
+
+    string itemName = "Gem";
+    uint   itemPrice = 3;
+
+    function beforeEach() public
+    {
+        chain = new SupplyChain();
+        initialBalance = 100;
+    }
+
+    function putItemForSale() public
+    {
+        chain.addItem(itemName, itemPrice);
+    }
 
     // Test for failing conditions in this contracts
     // test that every modifier is working
+    function testItemCanBePutOnSale() public {
+        putItemForSale();
+
+        uint expectedState = 0; // 0: Sale
+        uint expectedSku = 0;
+        address expectedBuyer = address(0);
+        address expectedSeller = this;
+
+        (
+        string memory name, uint sku, uint price, uint state, address seller, address buyer
+        )  = chain.fetchItem(expectedSku); // the first item
+
+        Assert.equal(sku, expectedSku, "Item sku is correct");
+        Assert.equal(name, itemName, "Item is named correctly");
+        Assert.equal(price, itemPrice, "Item price correctly");
+        Assert.equal(state, expectedState, "Item State is `For sale`");
+        Assert.equal(buyer, expectedBuyer, "Item buyer is 0x0");
+        Assert.equal(seller, expectedSeller, "Item seller is me");
+    }
+
+    function testUserDoesNotPaysTheRightPrice() public payable {
+        putItemForSale();
+        uint sku = 0;
+
+        // Need to differentiate buyer and seller
+        // this seems like transferring to self
+        // how to do this in solidity?
+
+        bool result = address(chain).call.value(0)(abi.encodeWithSignature("buyItem(uint)", sku));
+        Assert.isFalse(result, "under Paid for item");
+    }
+
+
+    function testUserPaysTheRightPrice() public {
+        putItemForSale();
+        uint sku = 0;
+
+        // Need to differentiate buyer and seller
+        // this seems like transferring to self
+        // how to do this in solidity?
+
+        bool result = address(chain).call.value(itemPrice+1)(abi.encodeWithSignature("buyItem(uint)", sku));
+        Assert.isTrue(result, "Paid the correct price...");
+    }
 
     // buyItem
 
@@ -24,8 +86,4 @@ contract TestSupplyChain {
 
     // test calling the function from an address that is not the buyer
     // test calling the function on an item not marked Shipped
-
-     
-
-
 }
